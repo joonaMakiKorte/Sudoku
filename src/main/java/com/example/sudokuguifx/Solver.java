8cddf70e0b70c0792f5b59cd7d80f1900b0c9177
Joona Maki-Korte <joona.maki-korte@tuni.fi>
2025-02-02 01:53:40 +0200

    Added auto-solver functionality

--- src/main/java/com/example/sudokuguifx/Solver.java
+++ src/main/java/com/example/sudokuguifx/Solver.java
@@ -1,25 +1,41 @@
 package com.example.sudokuguifx;
 
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
 public class Solver {
 
-    public void solveSudoku(int[][] board) {
-        if(board == null || board.length == 0)
-            return;
-        solve(board);
+    // Interface for notifying the UI of updates
+    public interface SolverCallback {
+        void onCellUpdated(int row, int col, int value) throws InterruptedException;
     }
 
-    public boolean solve(int[][] board){
+    public static boolean solveSudoku(int[][] board, SolverCallback callback) throws InterruptedException {
+        return solve(board, callback);
+    }
+
+    public static boolean solve(int[][] board, SolverCallback callback) throws InterruptedException {
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] == 0){
                     for(int c = 1; c <= 9; c++){//trial. Try 1 through 9
-                        if(isValid(board, i, j, c)){
+                        if(isValid(board, i, j, c)) {
                             board[i][j] = c; //Put c for this cell
 
-                            if(solve(board))
+                            if (callback != null) {
+                                callback.onCellUpdated(i,j,c);
+                            }
+
+                            if(solve(board,callback)) {
                                 return true; //If it's the solution return true
-                            else
-                                board[i][j] = 0; //Otherwise go back
+                            }
+
+                            board[i][j] = 0; //Otherwise go back
+
+                            // Notify the callback
+                            if (callback != null) {
+                                callback.onCellUpdated(i,j,0);
+                            }
                         }
                     }
 
@@ -30,7 +46,7 @@ public class Solver {
         return true;
     }
 
-    private boolean isValid(int[][] board, int row, int col, int c){
+    private static boolean isValid(int[][] board, int row, int col, int c){
         for(int i = 0; i < 9; i++) {
             if(board[i][col] != 0 && board[i][col] == c) return false; //check row
             if(board[row][i] != 0 && board[row][i] == c) return false; //check column
@@ -39,4 +55,5 @@ public class Solver {
         }
         return true;
     }
+
 }
